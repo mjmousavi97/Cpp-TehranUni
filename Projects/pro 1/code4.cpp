bda8036dd358e6b44ff1bc3b539f0d0499880885
Mohammad Javad Mousavi <mohammadjavad.mousavi97@gmail.com>
2025-08-19 23:44:41 +0330

    Add Q3 and Q4 in pro 1

---
+++ Projects/pro 1/code4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string previous="";
+    string current;
+
+    while(cin >> current)
+    {
+        if (previous == current)
+            cout << "Repeated Word: " << current << endl;
+        previous = current;
+    }
+}
